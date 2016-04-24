3b7aa147a1a8883422925611d625fb8b5c7e3bfe
Nathaniel Graff <nathaniel.graff@gmail.com>
2016-04-23 18:49:01 -0700

    Initialized Atmel Studio project

---
+++ AVRSpectrumAnalyzer/main.c
@@ -0,0 +1,18 @@
+/*
+ * AVRSpectrumAnalyzer.c
+ *
+ * Created: 2016-04-23 6:47:57 PM
+ * Author : natil
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
